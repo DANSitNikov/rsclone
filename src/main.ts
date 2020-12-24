0eb49f4078db1f1754949fc15d5e67336f0d8a20
Alisa-Pavlova <43580809+Alisa-Pavlova@users.noreply.github.com>
2020-12-24 17:45:46 +0300

    feat: add configs

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+// @ts-ignore
+import * as Phaser from 'phaser';
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+    title: 'Sample',
+
+    type: Phaser.AUTO,
+
+    scale: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+    },
+
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+        },
+    },
+
+    parent: 'game',
+    backgroundColor: '#000000',
+};
+
+export const main = new Phaser.Game(gameConfig);
