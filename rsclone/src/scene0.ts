fb4e9289430dcc166161a0ed88e567aeab0f19db
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-12-25 23:21:02 +0300

    feat: add loading screen

--- rsclone/src/scene0.ts
+++ rsclone/src/scene0.ts
@@ -12,12 +12,71 @@ export class Scene0 extends Phaser.Scene {
   constructor() {
     super(sceneConfig);
   }
-  public preload ()
-  {
+  public preload() {
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(670, 515, 320, 50);
+    const percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+      },
+    });
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+      },
+    });
+    const assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+        font: '18px monospace',
+      },
+    });
+    assetText.setOrigin(0.5, 0.5);
+    loadingText.setOrigin(0.5, 0.5);
+    percentText.setOrigin(0.5, 0.5);
+    this.load.on('progress', function(value) {
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(680, 525, 300 * value, 30);
+      percentText.setText(Number(value * 100) + '%');
+    });
+    this.load.on('fileprogress', function(file) {
+      assetText.setText('Loading asset: ' + file.key);
+    });
+    this.load.on('complete', function() {
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+    });
+
     this.load.image('bg', 'assets/world/bg.png');
-    this.load.atlas('playerWalk', 'assets/character/walk/playerWalk.png', 'assets/character/walk/playerWalk.json');
-    this.load.atlas('playerIdle', 'assets/character/idle/playerIdle.png', 'assets/character/idle/playerIdle.json');
-    this.load.atlas('playerJump', 'assets/character/jump/playerJump.png', 'assets/character/jump/playerJump.json');
+    this.load.atlas(
+      'playerWalk',
+      'assets/character/walk/playerWalk.png',
+      'assets/character/walk/playerWalk.json',
+    );
+    this.load.atlas(
+      'playerIdle',
+      'assets/character/idle/playerIdle.png',
+      'assets/character/idle/playerIdle.json',
+    );
+    this.load.atlas(
+      'playerJump',
+      'assets/character/jump/playerJump.png',
+      'assets/character/jump/playerJump.json',
+    );
     this.load.image('tree', 'assets/world/tree.png');
     this.load.image('house', 'assets/world/house.png');
     this.load.image('ground', 'assets/world/ground.png');
@@ -37,65 +96,64 @@ export class Scene0 extends Phaser.Scene {
 
     this.objects.create(centerX + 200, centerY + 150, 'house').refreshBody();
 
-    this.objects.create(centerX, centerY + 410 , 'ground')
+    this.objects.create(centerX, centerY + 410, 'ground');
 
     this.physics.add.collider(this.player, this.objects);
-
 
     this.anims.create({
       key: 'walk',
       frames: this.anims.generateFrameNames('playerWalk', {
-        start: 2, end: 8,
-        prefix: '', suffix: '.png'
+        start: 2,
+        end: 8,
+        prefix: '',
+        suffix: '.png',
       }),
       frameRate: 11,
-      repeat: -1
+      repeat: -1,
     });
 
     this.anims.create({
       key: 'idle',
       frames: this.anims.generateFrameNames('playerIdle', {
-        start: 1, end: 8,
-        prefix: '', suffix: '.png'
+        start: 1,
+        end: 8,
+        prefix: '',
+        suffix: '.png',
       }),
       frameRate: 6,
-      repeat: -1
+      repeat: -1,
     });
 
     this.anims.create({
       key: 'jump',
       frames: this.anims.generateFrameNames('playerJump', {
-        start: 1, end: 3,
-        prefix: '', suffix: '.png'
+        start: 1,
+        end: 3,
+        prefix: '',
+        suffix: '.png',
       }),
       frameRate: 6,
-      repeat: -1
+      repeat: -1,
     });
-
   }
 
   public update() {
     const cursors = this.input.keyboard.createCursorKeys();
     const speed = 400;
 
-
     if (cursors.left.isDown) {
       this.player.body.setVelocityX(-speed);
-      if(this.player.body.touching.down) this.player.anims.play('walk', true);
+      if (this.player.body.touching.down) this.player.anims.play('walk', true);
       this.player.flipX = true;
-
     } else if (cursors.right.isDown) {
-
       this.player.body.setVelocityX(speed);
-      if(this.player.body.touching.down) this.player.anims.play('walk', true);
+      if (this.player.body.touching.down) this.player.anims.play('walk', true);
       this.player.flipX = false;
-
     } else {
-      if(this.player.body.touching.down) {
+      if (this.player.body.touching.down) {
         this.player.anims.play('idle', true);
       }
       this.player.body.setVelocityX(0);
-
     }
 
     if (cursors.up.isDown && this.player.body.touching.down) {
