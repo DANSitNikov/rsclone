ac81074c568856ca0ed36122a6d8add0d74a1115
Saidazizkhon Akbarov <akbaroff5@gmail.com>
2020-12-26 22:20:21 +0500

    feat: enemy fading animation, player dying animation

--- rsclone/src/scene0.ts
+++ rsclone/src/scene0.ts
@@ -5,19 +5,47 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   key: 'Game',
 };
 var enemy;
+var enemyOnScene = false;
+
 export class Scene0 extends Phaser.Scene {
   private player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   private objects: Phaser.Physics.Arcade.StaticGroup;
 
   constructor() {
     super(sceneConfig);
-  }
+	}
+	//Add enemy to scene
+	public addEnemy() {
+		if (this.player.x > 500) {
+				if (!enemyOnScene) {
+					this.enemyFadeIn();
+					enemy.setCollideWorldBounds(true);
+				}
+				setTimeout(this.enemyStop, 900);
+		}	
+	}
+
+	//Enemy fade-in
+	public enemyFadeIn() {
+		enemy.setVelocityX(-300);
+	}
+	public enemyStop() {
+		enemy.setVelocityX(0);
+		enemyOnScene = true;
+	}
+
+	//Check player and enemy coordinates
+	public checkPlayerCoord() {
+		
+	}
+
   public preload ()
   {
     this.load.image('bg', 'assets/world/bg.png');
     this.load.atlas('playerWalk', 'assets/character/walk/playerWalk.png', 'assets/character/walk/playerWalk.json');
     this.load.atlas('playerIdle', 'assets/character/idle/playerIdle.png', 'assets/character/idle/playerIdle.json');
 		this.load.atlas('playerJump', 'assets/character/jump/playerJump.png', 'assets/character/jump/playerJump.json');
+		this.load.spritesheet('playerDie', 'assets/character/die/playerDie.png', { frameWidth: 395, frameHeight: 396 });
 		this.load.spritesheet('enemyWalk', 'assets/enemies/enemy.png', { frameWidth: 500, frameHeight: 500 });
 		this.load.image('enemy', 'assets/enemies/walk1.png');
     this.load.image('tree', 'assets/world/tree.png');
@@ -31,7 +59,7 @@ export class Scene0 extends Phaser.Scene {
     this.player = this.physics.add.sprite(400, 300, 'playerIdle');
 
     this.player.setBounce(0.02);
-    this.player.setCollideWorldBounds(true);
+    this.player.setCollideWorldBounds(false);
 
     this.objects = this.physics.add.staticGroup();
 
@@ -41,9 +69,8 @@ export class Scene0 extends Phaser.Scene {
 
 		this.physics.add.collider(this.player, this.objects);
 		
-		enemy = this.physics.add.sprite(800, 500, 'enemyWalk');
-		enemy.setCollideWorldBounds(true);
-		enemy.setBounce(0.05);
+		enemy = this.physics.add.sprite(2000, 500, 'enemyWalk');
+		this.physics.add.collider(enemy, this.objects);
 
 
     this.anims.create({
@@ -75,11 +102,20 @@ export class Scene0 extends Phaser.Scene {
       frameRate: 6,
       repeat: -1
 		});
+
+		this.anims.create({
+      key: 'die',
+      frames: this.anims.generateFrameNumbers('playerDie', {
+        start: 0, end: 8,
+      }),
+      frameRate: 6,
+      repeat: 0
+		});
 		
 		this.anims.create({
 			key: 'enemyWalk',
 			frames: this.anims.generateFrameNumbers('enemyWalk', {
-				start: 0, end: 2 }),
+				start: 0, end: 1 }),
 			frameRate: 3,
 			repeat: -1
 		});
@@ -92,12 +128,12 @@ export class Scene0 extends Phaser.Scene {
 
 
     if (cursors.left.isDown) {
-      this.player.body.setVelocityX(-speed);
+			this.player.body.setVelocityX(-speed);
       if(this.player.body.touching.down) this.player.anims.play('walk', true);
       this.player.flipX = true;
 
     } else if (cursors.right.isDown) {
-
+			this.addEnemy();
       this.player.body.setVelocityX(speed);
       if(this.player.body.touching.down) this.player.anims.play('walk', true);
       this.player.flipX = false;
@@ -117,5 +153,5 @@ export class Scene0 extends Phaser.Scene {
 		
 		//Enemy animation
 			enemy.anims.play('enemyWalk', true);
-  }
+	}
 }
