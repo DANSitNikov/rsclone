b540d27e927948393abd56276a72d64adf776880
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2021-01-07 17:30:41 +0300

    refactor: format

--- rsclone/src/scene1.ts
+++ rsclone/src/scene1.ts
@@ -2,9 +2,9 @@ import * as Phaser from 'phaser';
 import Player from './player';
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
-    active: false,
-    visible: false,
-    key: 'Scene1',
+  active: false,
+  visible: false,
+  key: 'Scene1',
 };
 
 export default class Scene1 extends Phaser.Scene {
@@ -13,17 +13,17 @@ export default class Scene1 extends Phaser.Scene {
     private player: Player;
 
     constructor() {
-        super(sceneConfig);
+      super(sceneConfig);
     }
 
     public create(): void {
-        const map = this.make.tilemap({ key: 'map1' });
-        const tileset = map.addTilesetImage('bg1', 'bg1');
-        this.groundLayer = map.createLayer('Background', tileset);
-        this.groundLayer.setCollisionByProperty({ collides: true });
-        this.matter.world.convertTilemapLayer(this.groundLayer);
-        this.matter.world.setBounds(0, 0, 1680, 1040);
-        this.player = new Player(this, 'Scene2', 350, 640);
-        this.sound.add('wind').play({ loop: true });
+      const map = this.make.tilemap({ key: 'map1' });
+      const tileset = map.addTilesetImage('bg1', 'bg1');
+      this.groundLayer = map.createLayer('Background', tileset);
+      this.groundLayer.setCollisionByProperty({ collides: true });
+      this.matter.world.convertTilemapLayer(this.groundLayer);
+      this.matter.world.setBounds(0, 0, 1680, 1040);
+      this.player = new Player(this, 'Scene2', 350, 640);
+      this.sound.add('wind').play({ loop: true });
     }
 }
