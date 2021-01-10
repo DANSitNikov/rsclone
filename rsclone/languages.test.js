847abdc9077cd355256c65339f2fb2a29f16dbbd
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2021-01-10 23:12:26 +0300

    feat: add jest, babel, test lang files

---
+++ rsclone/languages.test.js
@@ -0,0 +1,9 @@
+import en from './src/languages/en';
+import ru from './src/languages/ru';
+import chTr from './src/languages/chTr';
+
+test('language files are same length', () => {
+  const bool = Object.keys(en).length === Object.keys(ru).length && 
+    Object.keys(ru).length === Object.keys(chTr).length;
+  expect(bool).toBeTruthy();
+})
