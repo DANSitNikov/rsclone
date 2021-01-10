8e78dd72e0511d9d21036786fabf4a578e086a57
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2021-01-11 00:24:29 +0300

    feat: more language tests

--- rsclone/languages.test.js
+++ rsclone/languages.test.js
@@ -3,9 +3,27 @@ import ru from './src/languages/ru';
 import chTr from './src/languages/chTr';
 
 describe('language files check', () => {
+  const ruRegexp = /[а-яА-ЯЁё]/gm;
+  const chRegexp = /[\u3400-\u9FBF]/gm;
   test('language files are same length', () => {
     const bool = Object.keys(en).length === Object.keys(ru).length && 
       Object.keys(ru).length === Object.keys(chTr).length;
     expect(bool).toBeTruthy();
   })
+  test('no ru in en', () => {
+    expect(findLetters(en, ruRegexp)).toBeFalsy();
+  })
+  test('no ru in ch_tr', () => {
+    expect(findLetters(chTr, ruRegexp)).toBeFalsy();
+  })
+  test('no ch in en', () => {
+    expect(findLetters(en, chRegexp)).toBeFalsy();
+  })
+  test('no ch in ru', () => {
+    expect(findLetters(ru, chRegexp)).toBeFalsy();
+  })
 });
+
+function findLetters(langObj, regexp){
+  return Object.values(langObj).some(element => regexp.test(element));
+}
